5be23445f59b176330863220d702aa4714adbaf8
Scott Serage <xttocsurge51@gmail.com>
2017-10-02 13:59:23 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+        </nav>
+        <div className="home">
+        </div>
+        <div className="services">
+        </div>
+        <div className="portfolio">
+        </div>
+        <div className="about">
+        </div>
+        <div className="team">
+        </div>
+        <div className="contact">
+        </div>
+        <footer className="footer">
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
